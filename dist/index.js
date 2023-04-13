bd6ea606e6be74980ee885100c6831d4c0caf0bc
Fabrice <codinglone@gmail.com>
2023-04-13 10:56:10 +0200

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,66 @@
+"use strict";
+let id = 5;
+let company = "Google";
+let isPublished = true;
+let x = "";
+let numbers = [1, 2, 3, 4, 5];
+let arr = [1, true, { names: "codinglone" }];
+let person = [1, 'Chuzzi', true];
+let employee;
+employee = [
+    [1, 'hello'],
+    [2, 'Hi'],
+    [3, 'Morning'],
+];
+let num = 1;
+num = true;
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+const user = {
+    id: 1,
+    name: 'John'
+};
+let cid = 1;
+let customerId = cid;
+function addNumber(x, y) {
+    return x + y;
+}
+const log = (message) => {
+    console.log(message);
+};
+const student = {
+    names: "Fabrice",
+    age: 21
+};
+student.rollNo = 2089720;
+const p1 = 1;
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+class Person {
+    constructor(id, name) {
+        this.register = () => `${this.name} is now registered`;
+        this.id = id;
+        this.name = name;
+    }
+}
+const chuzzi = new Person(1, "Fabrice");
+class Employee extends Person {
+    constructor(id, name, position) {
+        super(id, name);
+        this.position = position;
+    }
+}
+const emp = new Employee(3, 'codinglone', 'Tech Lead');
+console.log(emp.register());
---
+++ src/index.ts
@@ -0,0 +1,133 @@
+let id: number = 5
+let company: string = "Google"
+let isPublished: boolean = true
+let x: any = ""
+
+let numbers: number[] = [1,2,3,4,5]
+let arr: any[] = [1,true, {names: "codinglone"}]
+
+// Tuple 
+let person: [number, string, boolean] = [1, 'Chuzzi', true]
+
+// Tuple Array
+let employee: [number, string][]
+
+employee = [
+    [1, 'hello'],
+    [2, 'Hi'],
+    [3, 'Morning'],
+
+]
+
+// Union 
+let num: number | null | boolean = 1
+
+num = true
+
+// Enum 
+enum Direction1 {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+enum Direction2 {
+    Up = 'Up',
+    Down = 'Down',
+    Left = "Left",
+    Right = "Right"
+}
+
+type User = {
+    id: number,
+    name: string
+}
+
+// Objects 
+const user: User = {
+    id: 1,
+    name: 'John'
+}
+
+
+// Type Assertion 
+let cid: any = 1
+// let customerId = <number>cid
+
+let customerId = cid as number
+
+// Functions 
+function addNumber(x: number, y: number): number {
+    return x + y
+}
+
+// Void Function
+const log = (message: string | number): void => {
+    console.log(message);
+    
+}
+
+// Interfaces
+interface studentInterface {
+    names: string
+    age: number
+    rollNo?: number
+    readonly id?: number
+}
+
+const student: studentInterface = {
+    names: "Fabrice",
+    age: 21
+}
+
+student.rollNo = 2089720
+
+type Point = number | string 
+const p1: Point = 1
+
+interface MathFunc {
+    (x: number, y: number): number
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y
+const sub: MathFunc = (x: number, y: number) => x - y
+
+// class interface
+interface PersonInterface {
+    id: number
+    name: string
+    register(): string
+}
+
+// Classes 
+class Person implements PersonInterface {
+    public id
+    public name
+
+    constructor(id: number, name: string){
+        this.id = id
+        this.name = name
+    }
+
+    register = () => `${this.name} is now registered`
+
+}
+
+const chuzzi = new Person(1, "Fabrice")
+
+// Subclasses
+class Employee extends Person {
+    position: string 
+
+    constructor(id: number, name: string, position: string){
+        super(id, name)
+        this.position = position
+    }
+}
+
+const emp = new Employee(3, 'codinglone', 'Tech Lead')
+
+console.log(emp.register());
+
+
